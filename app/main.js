b03a72a51c4fe72f63f4e8d1cdf5971319afeede
Stuart Williams <stuart.williams@bbc.co.uk>
2017-06-29 18:26:17 +0100

    Added react

--- app/main.js
+++ app/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+import React from 'react'
+import { render } from 'react-dom'
+
+const HelloWorld = () => (
+  <h1>Hello World</h1>
+)
+
+render(<HelloWorld />, document.getElementById('root'))
