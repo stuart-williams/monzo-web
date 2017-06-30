c849ed4c440aadfc293157c61344756fe7484e44
Stuart Williams <stuart.williams@bbc.co.uk>
2017-06-30 13:57:18 +0100

    Got rid of redux stuff from Account component

--- app/components/Account.js
+++ app/components/Account.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { gql, graphql } from 'react-apollo'
-import { connect } from 'react-redux'
 
-const initialAccount = { balance: {}, spend_today: 0 }
+const Account = ({ data }) => {
+  const { account } = data
 
-const Account = ({ accountId, data: { account = initialAccount } }) => (
-  <section>
-    <h1>{account.description}</h1>
-    <p>Type: {account.type}</p>
-    <p>Balance: {account.balance.balance}</p>
-    <p>Spent Today: {account.balance.spend_today}</p>
-  </section>
-)
+  if (!account) return <p>Loading...</p>
+
+  return (
+    <section>
+      <h1>{account.description}</h1>
+      <p>Type: {account.type}</p>
+      <p>Balance: {account.balance.balance}</p>
+      <p>Spent Today: {account.balance.spend_today}</p>
+    </section>
+  )
+}
 
 Account.propTypes = {
-  accountId: PropTypes.string.isRequired,
   data: PropTypes.object.isRequired
 }
 
-const AccountWithData = graphql(gql`
+export default graphql(gql`
   query($accountId: String!) {
     account(accountId: $accountId) {
       id
@@ -33,15 +35,9 @@ const AccountWithData = graphql(gql`
     }
   }
 `, {
-    options: ({ accountId }) => ({
+    options: ({ match }) => ({
       variables: {
-        accountId
+        accountId: match.params.accountId
       }
     })
   })(Account)
-
-const mapStateToProps = (state, { match }) => ({
-  accountId: match.params.accountId
-})
-
-export default connect(mapStateToProps)(AccountWithData)
