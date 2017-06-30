4fa9a96d0d8840a5780bafe35f42706afb4ee1a6
Stuart Williams <stuart.williams@bbc.co.uk>
2017-06-30 09:03:24 +0100

    Removed unused store

--- app/store.js
+++
@@ -1,9 +0,0 @@
-import reducers from './reducers'
-import { createStore, compose } from 'redux'
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-export default createStore(
-  reducers,
-  composeEnhancers()
-)
