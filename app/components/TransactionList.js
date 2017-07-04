de8733322b657cc2a9526e7c442377f9bacf2cc7
Stuart Williams <stuart.williams@bbc.co.uk>
2017-07-04 20:55:36 +0100

    Revert TransactionList change

--- app/components/TransactionList.js
+++ app/components/TransactionList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import { gql, graphql } from 'react-apollo'
@@ -55,39 +55,34 @@ const getTransaction = (transaction, onClick) => {
   )
 }
 
-class TransactionList extends Component {
-  render () {
-    const { data: { transactions }, classes } = this.props
-    const { router } = this.context
-
-    if (!transactions) {
-      return (
-        <div className={classes.progressCt}>
-          <CircularProgress className={classes.progress} />
-        </div>
-      )
-    }
-
-    const grouped = groupTransactionsByDate(transactions)
-    const sortedDates = Object.keys(grouped).sort((a, b) => b - a)
-    const onClick = (transactionId) => router.history.push(`/transaction/${transactionId}`)
-
+const TransactionList = ({ data: { transactions }, classes }, { router }) => {
+  if (!transactions) {
     return (
-      <List>
-        {sortedDates.map((date) => (
-          <div key={date}>
-            <ListSubheader>{formatDate(date)}</ListSubheader>
-            {grouped[date].map((transaction, i) => (
-              <div key={transaction.id}>
-                {getTransaction(transaction, onClick)}
-                {grouped[date][i + 1] && <Divider inset />}
-              </div>
-            ))}
-          </div>
-        ))}
-      </List>
+      <div className={classes.progressCt}>
+        <CircularProgress className={classes.progress} />
+      </div>
     )
   }
+
+  const grouped = groupTransactionsByDate(transactions)
+  const sortedDates = Object.keys(grouped).sort((a, b) => b - a)
+  const onClick = (transactionId) => router.history.push(`/transaction/${transactionId}`)
+
+  return (
+    <List>
+      {sortedDates.map((date) => (
+        <div key={date}>
+          <ListSubheader>{formatDate(date)}</ListSubheader>
+          {grouped[date].map((transaction, i) => (
+            <div key={transaction.id}>
+              {getTransaction(transaction, onClick)}
+              {grouped[date][i + 1] && <Divider inset />}
+            </div>
+          ))}
+        </div>
+      ))}
+    </List>
+  )
 }
 
 TransactionList.propTypes = {
